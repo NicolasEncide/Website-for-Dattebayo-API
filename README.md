fcca104e00c6f8aaefcceb5ead039d9b46a99825
Nicolas <niencide@gmail.com>
2025-04-19 00:49:18 -0300

    Formating Styles

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Website-for-Dattebayo-API
 This website was made by me as part of my faculty project. Here I integrate the Dattebayo API, which you can learn more about through this link: https://api-dattebayo.vercel.app/
 
-This is for educational purposes only.
+Big thanks for viniciusschuelter for sharing this amazing API!
+
+Made for educational purposes only.
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,14 @@
-
+export const normalizeString = function(string) {
+    return string.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+}
+export const capitalize = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+export const createElement = function(tag, text, attributes = {}) {
+    const element = document.createElement(tag);
+    element.textContent = text;
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    return element;
+}
